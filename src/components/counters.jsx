9d40b4679da06a847618961ad393cf9fd5145e71
Colton Randall <coltondominicrandall@gmail.com>
2022-05-27 12:48:40 +1200

    added state change function to remove components (#5)
    Co-authored-by: Colton Randall <colton.randall@xero.com>

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,22 +11,47 @@ class Counters extends Component {
     ],
   };
 
-  handleDelete = () => {
-    console.log("Event handler called");
+  handleDelete = (counterId) => {
+    // Using filter to get all counters except one with given id
+    const counters = this.state.counters.filter(
+      (counter) => counter.id !== counterId
+    );
+    // Now we have a new array - override counters prop with counters constant.
+    this.setState({ counters }); // can simplify to ({ counters })
+  };
+
+  handleReset = () => {
+    // Use map method to get each counter and reset its value
+    const counters = this.state.counters.map((eachCounter) => {
+      eachCounter.value = 0;
+      return eachCounter;
+    });
+    this.setState({ counters });
   };
 
   render() {
     return (
-      <div className='d-flex aligns-items-center justify-content-center'>
-        <div className='m-5 w-25 d-flex aligns-items-center justify-content-center border border-danger shadow p-3 mb-5 bg-light rounded'>
-          <div>
-            {this.state.counters.map((counter) => (
-              <Counter
-                key={counter.id}
-                onDelete={this.handleDelete}
-                value={counter.value}
-              />
-            ))}
+      <div>
+        <div className=" d-flex aligns-items-center justify-content-center">
+          <button
+            onClick={this.handleReset}
+            className="btn btn-primary btn sm m-2"
+          >
+            Reset
+          </button>
+        </div>
+        <div className="d-flex aligns-items-center justify-content-center">
+          <div className="m-5 w-25 d-flex aligns-items-center justify-content-center border border-danger shadow p-3 mb-5 bg-light rounded">
+            <div>
+              {this.state.counters.map((counter) => (
+                <Counter
+                  key={counter.id}
+                  value={counter.value}
+                  id={counter.id}
+                  onDelete={this.handleDelete}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
