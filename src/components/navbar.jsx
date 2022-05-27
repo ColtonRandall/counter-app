d7fbbe8dcabbe63fb99f766354d947faad2f06f3
Colton Randall <colton.randall@xero.com>
2022-05-27 14:45:33 +1200

    restructured navbar.js as a sfc instead of class

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,39 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar{" "}
-            <span className="badge badge-pill badge-secondary">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+// stateless functional component - Can use SFC or class, personal pereference
+// must add props as a parameter
+// remove any instances of `this` from within the return()
+
+const NavBar = (props) => {
+  return (
+    <nav className="navbar bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {props.totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+// class NavBar extends Component {
+//   state = {};
+//   render() {
+//     return (
+//       <nav className="navbar bg-light">
+//         <div class="container-fluid">
+//           <a class="navbar-brand" href="#">
+//             Navbar{" "}
+//             <span className="badge badge-pill badge-secondary">
+//               {this.props.totalCounters}
+//             </span>
+//           </a>
+//         </div>
+//       </nav>
+//     );
+//   };
 
 export default NavBar;
