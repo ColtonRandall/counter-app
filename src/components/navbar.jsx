16afd0cd5f6d25752e1330bab4a22ecabb3c4d38
Colton Randall <coltondominicrandall@gmail.com>
2022-05-27 14:11:26 +1200

    Multiple components feature (#7)
    * added state change function to remove components
    
    * refactored to single source of truth
    
    * refactored for App.js to be parent component
    
    Co-authored-by: Colton Randall <colton.randall@xero.com>

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav className="navbar bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
